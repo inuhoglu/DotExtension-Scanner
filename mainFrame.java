a235343b2c9662181b9e6cced3840ec2d290dbe0
inuhoglu <inuh@hotmail.com>
2015-07-14 13:33:57 +0300

    Scan if your files have specific word in them

---
+++ mainFrame.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @(#)mainFrame.java
+ *	Main frame to drive GUI
+ *
+ * @Ilkcan Nuhoglu
+ * @version 1.00 2015/7/13
+ */
+
+
+public class mainFrame {
+
+    public static void main(String[] args) {
+    	JFrame mainFrame=new JFrame("DotExtention Scanner");
+    	mainFrame.getContentPane().add(new mainPanel());
+    	mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	mainFrame.pack();
+    	mainFrame.setVisible(true);
+    }
+    
+    
+}
---
+++ mainPanel.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+/**
+ * @(#)mainPanel.java
+ *
+ *
+ * @Ilkcan Nuhoglu
+ * @version 1.00 2015/7/13
+ */
+
+
+public class mainPanel extends JPanel implements Observer {
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();	//Set approprite resolution
+	final int XDIMENSION=(int)screenSize.getWidth()*3/5;
+	final int YDIMENSION=(int)screenSize.getHeight()*3/5;
+	optionsPanel oP=new optionsPanel();  
+	JPanel iP=new JPanel();
+		
+    public mainPanel() {
+    	super(new BorderLayout() );  	
+    	setPreferredSize(new Dimension(XDIMENSION,YDIMENSION));
+    	oP.addObserver(this);	//Adding observer to button in optionsPanel
+    	add(oP.getPanel(),BorderLayout.NORTH);
+    	add(iP);
+    }
+    
+    public void update(Observable o, Object arg){
+    	String temp="";
+    	for(int i=0;i<oP.getFiles().size();i++)
+    		temp+="\n"+oP.getFiles().get(i);	//Add file names to text area
+    	JTextArea tA=new JTextArea(temp);
+    	JScrollPane sP=new JScrollPane(tA);	
+    	sP.setPreferredSize(new Dimension(XDIMENSION,YDIMENSION-oP.getPanel().getPreferredSize().height-10));
+    	iP.add(sP);
+    	updateUI();	//refresh interface
+    	repaint();
+    }    
+}
